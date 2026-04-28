20ffd217c6dbd6ef8cf856031c836ddadb63c62a
Uyenochris <102437571+Uyenochris@users.noreply.github.com>
2026-04-28 02:10:45 -0400

    Add files via upload

---
+++ js/food.js
@@ -0,0 +1,38 @@
+export function foodPage() {
+    return `
+      <section class="food">
+  
+        <div class="food-hero">
+          <div class="food-overlay">
+            <div class="food-title">
+              <div class=""></div>
+              <h1>1.<span> food festival</span></h1>
+            </div>
+          </div>
+        </div>
+  
+        <div class="food-content">
+          <div class="food-col">
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+              totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+              totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            </p>
+          </div>
+  
+          <div class="food-col">
+            <p>
+              Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+            </p>
+          </div>
+        </div>
+  
+      </section>
+    `;
+  }
+
+
+
+
+ 
---
+++ js/speech.js
@@ -0,0 +1,43 @@
+export function speechPage() {
+    return `
+      <section class="speech">
+  
+        <div class="speech-hero">
+          <div class="speech-overlay">
+            <h1><span class="number">3.</span> speech</h1>
+          </div>
+        </div>
+  
+        <div class="speech-content">
+          <div class="speech-col">
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+              totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+            </p>
+          </div>
+  
+          <div class="speech-col">
+            <p>
+              Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+            </p>
+          </div>
+        </div>
+  
+      </section>
+    `;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
