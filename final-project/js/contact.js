9688cbb1fa49b7cce2423bcca1af98b8ed731355
Uyenochris <102437571+Uyenochris@users.noreply.github.com>
2026-04-28 02:04:21 -0400

    Add files via upload

---
+++ final-project/js/contact.js
@@ -0,0 +1,33 @@
+export function contactPage() {
+    return `
+      <section class="contact">
+  
+        <h2>CONTACT US:</h2>
+        <div class="line dark"></div>
+  
+        <div class="contact-wrapper">
+  
+          <!-- LEFT: FORM -->
+          <div class="contact-form">
+            <input type="text" placeholder="Your name..." />
+            <input type="text" placeholder="Email Address..." />
+            <input type="text" placeholder="Company..." />
+            <textarea placeholder="Message..."></textarea>
+  
+            <button>SEND MESSAGE</button>
+          </div>
+  
+          <!-- RIGHT: MAP -->
+          <div class="contact-map"></div>
+  
+        </div>
+  
+      </section>
+    `;
+  }
+
+
+
+
+
+
---
+++ final-project/js/dj.js
@@ -0,0 +1,39 @@
+export function djPage() {
+    return `
+      <section class="dj">
+  
+        <div class="dj-hero">
+          <div class="dj-overlay">
+            <div class="dj-title">
+              <h1>2.<span> dee-jay</span></h1>
+            </div>
+          </div>
+        </div>
+  
+        <div class="dj-content">
+          <div class="dj-col">
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+              totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+              qui ratione voluptatem sequi nesciunt.
+            </p>
+          </div>
+  
+          <div class="dj-col">
+            <p>
+              Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+            </p>
+          </div>
+        </div>
+  
+      </section>
+    `;
+  }
+
+
+
+
+
+
